95b5d8c85752aa9127bd736f7065fff1a1caf866
Jesse McKinney <jessemckinney513@gmail.com>
2020-07-21 13:36:04 -0400

    yay

--- src/SkinnyFooter/SkinnyFooter.jsx
+++ src/SkinnyFooter/SkinnyFooter.jsx
@@ -12,43 +12,42 @@ class SkinnyFooter extends Component {
   render() {
     return (
       <>
-        <Container className="skinnyFooter">
-          <Row>
-            <Col>
-              <ul>
-                <li>
-                  <h5>The World Of React 1</h5>
-                </li>
-                <li>
-                  <h5>The World Of React 2</h5>
-                </li>
-                <li>
-                  <h5>The World Of React 3</h5>
-                </li>
-                <li>
-                  <h5>The World Of React 4</h5>
-                </li>
-              </ul>
-            </Col>
-
-            <Col>
-              <ul>
-                <li>
-                  <h5>The World Of React 5</h5>
-                </li>
-                <li>
-                  <h5>The World Of React 6</h5>
-                </li>
-                <li>
-                  <h5>The World Of React 7</h5>
-                </li>
-                <li>
-                  <h5>The World Of React 8</h5>
-                </li>
-              </ul>
-            </Col>
-          </Row>
-        </Container>
+      <Container className="skinnyFooter">
+      <Row>
+        <Col>
+          <ul>
+            <li>
+              <h5>The World Of React 1</h5>
+            </li>
+            <li>
+              <h5>The World Of React 2</h5>
+            </li>
+            <li>
+              <h5>The World Of React 3</h5>
+            </li>
+            <li>
+              <h5>The World Of React 4</h5>
+            </li>
+          </ul>
+        </Col>
+        <Col>
+          <ul>
+            <li>
+              <h5>The World Of React 5</h5>
+            </li>
+            <li>
+              <h5>The World Of React 6</h5>
+            </li>
+            <li>
+              <h5>The World Of React 7</h5>
+            </li>
+            <li>
+              <h5>The World Of React 8</h5>
+            </li>
+          </ul>
+        </Col>
+      </Row>
+    </Container>
       </>
     );
   }
