5026548705cf2842cd064160ae1719f6c334173a
Jesse McKinney <jessemckinney513@gmail.com>
2020-07-22 11:45:09 -0400

    yay

--- src/SkinnyFooter/SkinnyFooter.jsx
+++ src/SkinnyFooter/SkinnyFooter.jsx
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import Dropdown from "react-bootstrap/Dropdown";
 import "./SkinnyFooter.css";
 
 class SkinnyFooter extends Component {
@@ -47,25 +48,38 @@ class SkinnyFooter extends Component {
     return (
       <>
         <div id="footerWrapper">
-          <Container className="openedBottomMenu">
-            <Row>
-              <Col></Col>
-              <Col></Col>
-            </Row>
-          </Container>
-
           <Container className="closedSkinnyFooter">
             <Row>
               <Col>
-                <div
-                  id="menuIconContainer"
-                  className={this.state.changeNavBar ? "change" : ""}
-                  onClick={this.toggleMenu}
-                >
-                  <div className="bar1"></div>
-                  <div className="bar2"></div>
-                  <div className="bar3"></div>
-                </div>
+                <Dropdown drop={"up"} key={"up"}>
+                  <Dropdown.Toggle
+                    className="dropDownButton"
+                    variant="light"
+                    id="dropdown-basic"
+                  >
+                    <div
+                      id="menuIconContainer"
+                      className={this.state.changeNavBar ? "change" : ""}
+                      onClick={this.toggleMenu}
+                    >
+                      <div className="bar1"></div>
+                      <div className="bar2"></div>
+                      <div className="bar3"></div>
+                    </div>
+                  </Dropdown.Toggle>
+
+                  <Dropdown.Menu>
+                    <Dropdown.Item href="#/action-1">
+                      First Action
+                    </Dropdown.Item>
+                    <Dropdown.Item href="#/action-2">
+                      Another action
+                    </Dropdown.Item>
+                    <Dropdown.Item href="#/action-3">
+                      Something else
+                    </Dropdown.Item>
+                  </Dropdown.Menu>
+                </Dropdown>
               </Col>
             </Row>
           </Container>
