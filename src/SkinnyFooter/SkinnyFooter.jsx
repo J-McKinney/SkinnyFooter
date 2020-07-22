fc8e846b8adcdb76450cd184a1b5e6c6a538d54f
Jesse McKinney <jessemckinney513@gmail.com>
2020-07-22 12:05:16 -0400

    yay

--- src/SkinnyFooter/SkinnyFooter.jsx
+++ src/SkinnyFooter/SkinnyFooter.jsx
@@ -44,14 +44,14 @@ class SkinnyFooter extends Component {
                     </div>
                   </Dropdown.Toggle>
 
-                  <Dropdown.Menu>
-                    <Dropdown.Item href="#/action-1">
+                  <Dropdown.Menu className="dropdownMenu">
+                    <Dropdown.Item className="dropdownItem" href="#/action-1">
                       First Action
                     </Dropdown.Item>
-                    <Dropdown.Item href="#/action-2">
+                    <Dropdown.Item className="dropdownItem" href="#/action-2">
                       Another action
                     </Dropdown.Item>
-                    <Dropdown.Item href="#/action-3">
+                    <Dropdown.Item className="dropdownItem" href="#/action-3">
                       Something else
                     </Dropdown.Item>
                   </Dropdown.Menu>
